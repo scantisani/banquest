4b8854e557c271c059ac0f872a7acb1d2e6f8594
Scott Cantisani <scantisani@gmail.com>
2014-11-29 04:15:44 +0000

    More descriptive variable name: changed 'map_array' to 'squares'

--- lib/game/map.rb
+++ lib/game/map.rb
@@ -1,14 +1,16 @@
 # Generates the map for each game.
 class Map
   def initialize
-    @map_array = Array.new(10) { Array.new(10, '.') }
+    @squares = Array.new(10) { Array.new(10, '.') }
   end
 
   def to_string
     @map_string = ''
-    @map_array.each do |row|
+    @squares.each do |row|
       @map_string += row.join + "\n"
     end
     @map_string
   end
+
+  attr_accessor :squares
 end
