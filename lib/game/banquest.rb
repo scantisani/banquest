4dd72ac69e6fc0ebf73a86e98da483d30f070083
Scott Cantisani <scantisani@gmail.com>
2014-11-29 04:16:53 +0000

    New player class that has coordinates; these coordinates place it in the map array

--- lib/game/banquest.rb
+++ lib/game/banquest.rb
@@ -1,7 +1,15 @@
 require_relative 'map'
+require_relative 'player'
 
 # The main game loop
 class Banquest
   map = Map.new
+  @player = Player.new
+
+  i = @player.location[0]
+  j = @player.location[1]
+
+  map.squares[i][j] = '@'
+
   print map.to_string
 end
---
+++ lib/game/player.rb
@@ -0,0 +1,8 @@
+# The player.
+class Player
+  def initialize
+    @location = [3, 5]
+  end
+
+  attr_accessor :location
+end
