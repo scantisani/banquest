b1a0e3224918331a64a23e890bd33876576cdd97
Scott Cantisani <scantisani@gmail.com>
2014-11-29 04:19:06 +0000

    Made map private to banquest instance

--- lib/game/banquest.rb
+++ lib/game/banquest.rb
@@ -3,13 +3,13 @@ require_relative 'player'
 
 # The main game loop
 class Banquest
-  map = Map.new
+  @map = Map.new
   @player = Player.new
 
   i = @player.location[0]
   j = @player.location[1]
 
-  map.squares[j][i] = '@'
+  @map.squares[j][i] = '@'
 
   print map.to_string
 end
