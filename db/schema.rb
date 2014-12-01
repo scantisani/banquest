33f27a211dc781e388e6bbbe8f6400a676b20140
Scott Cantisani <scantisani@gmail.com>
2014-12-01 12:09:42 +0000

    Added save_data field to model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129022635) do
+ActiveRecord::Schema.define(version: 20141201111618) do
 
   create_table "games", force: true do |t|
     t.string   "keypress"
     t.text     "map"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "save_data"
   end
 
 end
