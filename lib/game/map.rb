933c491986946b63eb595369871ed1a77356f74b
scantisani <scantisani@gmail.com>
2014-12-09 10:58:41 +0000

    Corridors now connect randomly placed rooms.

--- lib/game/map.rb
+++ lib/game/map.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'room'
+require_relative 'wall'
 
 # Generates the map for each game.
 class Map
@@ -10,24 +11,37 @@ class Map
     seed)
     @rng = Random.new(seed)
     # the dungeon
-    @structure = Array.new(MAP_SIZE) { Array.new(MAP_SIZE, Square.new) }
-    @rooms = make_rooms
+    @structure = Array.new(MAP_SIZE) { Array.new(MAP_SIZE, Wall.new) }
+    @rooms = []
+
+    generate
 
     # things in the dungeon
     # @occupying_dungeon = Array.new(10) { Array.new(10, '') }
     # @player = player
   end
 
+  def to_string
+    rows = @structure.collect { |row| row.collect(&:symbol).join }
+    rows.join("\n")
+  end
+
+  private
+
+  def generate
+    make_rooms
+    place_rooms
+    connect_rooms
+  end
+
   def make_rooms
-    rooms = []
-    @rng.rand(3..6).times { rooms << Room.new(@rng.rand(9999)) }
-    rooms
+    @rng.rand(8..10).times { @rooms << Room.new(@rng.rand(9999)) }
   end
 
   def place_rooms
     @rooms.each do |room|
-      origin = new_origin(room.length, room.width)
-      x, y = origin[0], origin[1]
+      room.origin = new_origin(room.length, room.width)
+      x, y = room.origin[:x], room.origin[:y]
 
       room.plan.each_index do |i|
         room.plan[i].each_with_index do |square, j|
@@ -38,21 +52,54 @@ class Map
   end
 
   def new_origin(length, width)
-    # TODO: add detection for collisions
-    x = y = 3
-    until @structure[y][x].symbol == ' '
-      x = @rng.rand(0..MAP_SIZE - 1 - width)
-      y = @rng.rand(0..MAP_SIZE - 1 - length)
+    # TODO: add detection for collisions?
+    x = y = 1
+    until @structure[y][x].is_a? Wall
+      x = @rng.rand(1..MAP_SIZE - 1 - width)
+      y = @rng.rand(1..MAP_SIZE - 1 - length)
     end
-    [x, y]
+    { x: x, y: y }
   end
 
-  def to_string
-    rows = @structure.collect { |row| row.collect(&:symbol).join }
-    rows.join("\n")
+  def connect_rooms
+    unconnected_rooms = @rooms.clone
+    connected_rooms = [@rooms[1..-1].sample(random: @rng)]
+
+    until unconnected_rooms.empty?
+      unconnected_rooms.each do |room|
+        other_rooms = connected_rooms.reject { |x| x == room }
+        close_room = closest_room(room, other_rooms)
+
+        make_corridor(room, close_room)
+        unconnected_rooms.reject! { |x| x == room || x == close_room }
+      end
+    end
   end
 
-  private
+  def closest_room(room, compared_rooms)
+    compared_rooms.min_by { |x| euclidean_distance(room.origin, x.origin) }
+  end
+
+  def euclidean_distance(p, q)
+    Math.sqrt((p[:x] - q[:x])**2 + (p[:y] - q[:y])**2)
+  end
+
+  def make_corridor(room_1, room_2)
+    start = random_room_square(room_1)
+    finish = random_room_square(room_2)
+
+    x_range = [start[:x], finish[:x]].min..[start[:x], finish[:x]].max
+    y_range = [start[:y], finish[:y]].min..[start[:y], finish[:y]].max
+
+    x_range.each { |x| @structure[start[:y]][x] = Floor.new }
+    y_range.each { |y| @structure[y][finish[:x]] = Floor.new }
+  end
+
+  def random_room_square(room)
+    x = room.origin[:x] + @rng.rand(0..room.width - 1)
+    y = room.origin[:y] + @rng.rand(0..room.length - 1)
+    { x: x, y: y }
+  end
 
   def place_occupants
     clear_occupants
--- lib/game/room.rb
+++ lib/game/room.rb
@@ -1,6 +1,4 @@
-require_relative 'door'
 require_relative 'floor'
-require_relative 'wall'
 
 # A room in the dungeon.
 class Room
@@ -9,48 +7,14 @@ class Room
 
     @length = @rng.rand(4..12) # y-axis
     @width = @rng.rand(4..12) # x-axis
+    @origin = { x: 0, y: 0 }
 
     @plan = Array.new(@length) { Array.new(@width, Floor.new) }
-    make_structure
   end
 
   attr_reader :plan
   attr_reader :length
   attr_reader :width
 
-  private
-
-  def make_structure
-    make_walls
-    make_doors
-  end
-
-  def make_walls
-    @plan[0].fill(Wall.new)
-    @plan[1..-2].each do |row|
-      row[0] = Wall.new
-      row[-1] = Wall.new
-    end
-    @plan[-1].fill(Wall.new)
-  end
-
-  def make_doors
-    # array starts at 0, so we need -1 for last value, -2 for 2nd-last value
-    @rng.rand(1..3).times do
-      if either(true, false)
-        # east or west walls
-        x = either(0, @width - 1)
-        y = @rng.rand(1..@length - 2) # we don't want doors in corners
-      else
-        # north or south walls
-        x = @rng.rand(1..@width - 2) # we don't want doors in corners
-        y = either(0, @length - 1)
-      end
-      @plan[y][x] = Door.new
-    end
-  end
-
-  def either(first, second)
-    [first, second].sample(random: @rng)
-  end
+  attr_accessor :origin
 end
