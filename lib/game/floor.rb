20b57306a119bb7f3cfbe8c1f34c3c9e15eecbab
Scott Cantisani <scantisani@gmail.com>
2014-12-10 08:23:02 +0000

    Moved 'occupant' functionality to floor.rb only

--- lib/game/floor.rb
+++ lib/game/floor.rb
@@ -3,6 +3,15 @@ require_relative 'square'
 # A floor square in the dungeon.
 class Floor < Square
   def initialize
-    @symbol = '.'
+    @character = '.'
+    @symbol = @character
+    @occupant = nil
+  end
+
+  attr_reader :occupant
+
+  def occupy(actor)
+    @occupant = actor
+    actor ? @symbol = actor.symbol : @symbol = @character
   end
 end
--- lib/game/square.rb
+++ lib/game/square.rb
@@ -1,16 +1,7 @@
 # A single square of the dungeon
 class Square
   def initialize
-    @core_symbol = ' '
-    @symbol = @core_symbol
-    @occupant = nil
+    @symbol = ' '
   end
-
   attr_reader :symbol
-  attr_reader :occupant
-
-  def occupy(actor)
-    @occupant = actor
-    actor ? @symbol = actor.symbol : @symbol = @core_symbol
-  end
 end
