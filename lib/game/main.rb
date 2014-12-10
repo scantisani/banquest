92e839ab86d5c1eb252701d4da7c693a6cf5bfe8
Scott Cantisani <scantisani@gmail.com>
2014-12-10 09:43:37 +0000

    The seed is now part of save game data

--- lib/game/main.rb
+++ lib/game/main.rb
@@ -4,9 +4,10 @@ require 'yaml'
 
 # The main game loop
 class Main
-  def initialize
+  def initialize(seed = Random.new.rand(9999))
+    @seed = seed
     @player = Player.new
-    @map = PopulatedMap.new(@player, Random.new.rand(9999))
+    @map = PopulatedMap.new(@player, @seed)
   end
 
   def keypress(key)
@@ -21,7 +22,7 @@ class Main
   end
 
   def save_game
-    data = { player_x: @player.x, player_y: @player.y }
+    data = { player_x: @player.x, player_y: @player.y, seed: @seed }
     data.to_yaml
   end
 
@@ -29,5 +30,7 @@ class Main
     loaded_data = YAML.load(data)
     @player.x = loaded_data[:player_x]
     @player.y = loaded_data[:player_y]
+    @seed = loaded_data[:seed]
+    @map = PopulatedMap.new(@player, @seed)
   end
 end
