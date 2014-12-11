3197e565c6ffbeeaaaba1461a40fe784d435bf15
Scott Cantisani <scantisani@gmail.com>
2014-12-11 15:44:08 +0000

    Added two tests for map.rb

--- test/map_test.rb
+++ test/map_test.rb
@@ -3,13 +3,25 @@ require_relative '../lib/game/map.rb'
 
 # Unit tests for map.rb
 class MapTest < Test::Unit::TestCase
-  def test_draw
-    # will need changing later--blank map format will be different
-    map = Map.new(Player.new(5, 5))
-    expected_map = ''
-    5.times { expected_map += '..........<br>' }
-    expected_map += '.....@....<br>'
-    4.times { expected_map += '..........<br>' }
-    assert_equal(expected_map, map.draw)
+  def test_outer_walls
+    map = Map.new(Random.rand(9999))
+
+    map.structure[0..2].each { |row| row.each { |square| assert(square.is_a? Wall) } }
+    map.structure.each { |row| row[0..2].each { |square| assert(square.is_a? Wall) } }
+
+    map.structure[-3..-1].each { |row| row.each { |square| assert(square.is_a? Wall) } }
+    map.structure.each { |row| row[-3..-1].each { |square| assert(square.is_a? Wall) } }
+  end
+
+  def test_seeded_generation
+    # is there a better way to do this?
+    seed = Random.rand(9999)
+    map_1 = Map.new(seed)
+    map_2 = Map.new(seed)
+    map_1.structure.each_with_index do |row, y|
+      row.each_with_index do |square, x|
+        assert(map_2.structure[y][x].is_a? square.class)
+      end
+    end
   end
 end
