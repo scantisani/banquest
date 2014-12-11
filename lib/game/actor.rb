277467335cbff9a51b3229442f5c27883e0b979a
Scott Cantisani <scantisani@gmail.com>
2014-12-11 17:38:10 +0000

    Added Actor superclass for the player and monsters

---
+++ lib/game/actor.rb
@@ -0,0 +1,22 @@
+# The player or a monster
+class Actor
+  def initialize(x = 0, y = 0)
+    @x = x
+    @y = y
+    @symbol = ''
+  end
+
+  attr_accessor :x
+  attr_accessor :y
+  attr_reader :symbol
+
+  def move(direction)
+    compass = {
+      north: [0, -1], east: [1, 0], south: [0, 1], west: [-1, 0],
+      northeast: [1, -1], southeast: [1, 1], southwest: [-1, 1],
+      northwest: [-1, -1]
+    }
+    @x += compass[direction][0] # x-axis movement
+    @y += compass[direction][1] # y-axis movement
+  end
+end
