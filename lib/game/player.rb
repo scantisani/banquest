20652483245b8e95245fa34d759c440fa44a3001
Scott Cantisani <scantisani@gmail.com>
2014-12-15 22:12:11 +0000

    Removed line that reset player health to 20 every turn

--- lib/game/player.rb
+++ lib/game/player.rb
@@ -5,7 +5,6 @@ class Player < Actor
   def initialize(map, data = { x: 0, y: 0, hit_points: 20 })
     super
     @symbol = '@'
-    @hit_points = 20
     @damage_range = 1..6
     map.add_actor(self)
   end
