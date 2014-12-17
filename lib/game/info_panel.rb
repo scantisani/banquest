8fc8f8bcea243064856f4a2651715eb656cd38c1
scantisani <scantisani@gmail.com>
2014-12-17 15:37:36 +0000

    Inventory items now show up on the display

--- lib/game/info_panel.rb
+++ lib/game/info_panel.rb
@@ -7,7 +7,10 @@ class InfoPanel
     @contents = DEFAULT_CONTENTS.clone
   end
 
-  DEFAULT_CONTENTS = Array.new(10) { '|' + (' ' * 39) } +
+  DEFAULT_CONTENTS = ['|' + '-' * 39] +
+                     ['| INVENTORY'] +
+                     ['|' + '-' * 39] +
+                     Array.new(7) { '|' + (' ' * 39) } +
                      ['|' + '-' * 39] +
                      ['| COMBAT'] +
                      ['|' + '-' * 39] +
@@ -22,12 +25,26 @@ class InfoPanel
   def redraw
     @contents = DEFAULT_CONTENTS.dup
 
+    draw_inventory
+    draw_status_messages
+    draw_combat_messages
+  end
+
+  def draw_inventory
+    @player.inventory.each_with_index do |item, i|
+      @contents[3 + i] = "| #{item.quantity} #{item.name}"
+    end
+  end
+
+  def draw_combat_messages
     combat_messages = @monsters.map(&:combat_message) + [@player.combat_message]
     combat_messages.compact!
     combat_messages.each_with_index do |message, i|
       @contents[13 + i] = "| #{message}"
     end
+  end
 
+  def draw_status_messages
     @contents[21] = "| Health: #{@player.hit_points}"
     @contents[22] = "| #{@player.hunger_message}"
   end
