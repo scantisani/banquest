27fdedae818f45605a06e331c9db75b4591e29bb
scantisani <scantisani@gmail.com>
2014-12-17 12:35:05 +0000

    Player now hungers with each step

--- lib/game/info_panel.rb
+++ lib/game/info_panel.rb
@@ -4,19 +4,31 @@ class InfoPanel
     @map = map
     @player = map.player
     @monsters = map.monsters
-    @contents = Array.new(40) { '|' + (' ' * 39) }
+    @contents = DEFAULT_CONTENTS.clone
   end
+
+  DEFAULT_CONTENTS = Array.new(10) { '|' + (' ' * 39) } +
+                     ['|' + '-' * 39] +
+                     ['| COMBAT'] +
+                     ['|' + '-' * 39] +
+                     Array.new(5) { '|' + (' ' * 39) } +
+                     ['|' + '-' * 39] +
+                     ['| STATUS'] +
+                     ['|' + '-' * 39] +
+                     Array.new(19) { '|' + (' ' * 39) }
 
   attr_reader :contents
 
   def redraw
-    @contents = Array.new(40) { '|' + (' ' * 39) }
+    @contents = DEFAULT_CONTENTS.dup
 
     combat_messages = @monsters.map(&:combat_message) + [@player.combat_message]
     combat_messages.compact!
-    @contents[10] = "| Health: #{@player.hit_points}"
     combat_messages.each_with_index do |message, i|
-      @contents[11 + i] = "| #{message}"
+      @contents[13 + i] = "| #{message}"
     end
+
+    @contents[21] = "| Health: #{@player.hit_points}"
+    @contents[22] = "| #{@player.hunger_message}"
   end
 end
--- lib/game/player.rb
+++ lib/game/player.rb
@@ -2,11 +2,12 @@ require_relative 'actor'
 
 # The player
 class Player < Actor
-  def initialize(map, data = { x: 0, y: 0, hit_points: 20 })
+  def initialize(map, data = { x: 0, y: 0, hit_points: 20, hunger: 100 })
     super
     @symbol = '@'
     @damage_range = 1..6
     @map.player = self
+    @hunger = data[:hunger]
   end
 
   ATTACK_MESSAGES = ['You gnaw away at the NAME!',
@@ -23,6 +24,17 @@ class Player < Actor
                    'You reduce the NAME to bones and gristle.',
                    'You slurp up the last of the NAME.']
 
+  HUNGER_MESSAGES = ['You are wasting away from lack of food!',
+                     'You are starving!',
+                     'You are ravenous!',
+                     'You are positively famished.',
+                     'You could eat a whole horse.',
+                     'Your belly rumbles.',
+                     'You are definitely getting hungry.',
+                     'You feel a little peckish.',
+                     'You are full.',
+                     'You are absolutely stuffed.']
+
   def attack(actor)
     super
     if actor.hit_points > 0
@@ -34,11 +46,25 @@ class Player < Actor
   end
 
   def move(direction)
+    increase_hunger
+
     position = potential_move(direction)
     return if position.nil?
 
     new_x, new_y = position[:x], position[:y]
     return (@x, @y = new_x, new_y) unless @map.structure[new_y][new_x].occupied?
     attack(@map.structure[new_y][new_x].occupant)
+  end
+
+  def hunger_message
+    HUNGER_MESSAGES[@hunger / 20]
+  end
+
+  def save_data
+    { x: @x, y: @y, hit_points: @hit_points, hunger: @hunger }
+  end
+
+  def increase_hunger
+    @hunger -= 1 if @hunger > 0
   end
 end
